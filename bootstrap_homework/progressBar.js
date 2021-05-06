92588c9a01511af6f1455a5ad82dbf45f8527434
kate-vovk <kateryna.vovk@nure.ua>
2021-05-06 23:21:24 +0300

    Bootstrap homework

---
+++ bootstrap_homework/progressBar.js
@@ -0,0 +1,6 @@
+function scrollPercentage(){
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+};
